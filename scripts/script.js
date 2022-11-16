7e7e9e1a2403547f84e464362dd2aa4173222fc6
Timur R <timurreziapov2@gmail.com>
2022-11-16 13:55:11 -0800

    shopping practice

--- scripts/script.js
+++ scripts/script.js
@@ -13,16 +13,30 @@
 // greet("Timur", "Night", "Dinner");
 // greet();
 
-const circle = (circleRadius) => {
-    let area = Math.PI * circleRadius ** 2;
-    return area.toFixed(2);
+// const circle = (circleRadius) => {
+//     let area = Math.PI * circleRadius ** 2;
+//     return area.toFixed(2);
+// }
+
+// let findArea = prompt("Enter the radius");
+// alert(`The area of a circle with radius ${findArea} is ${circle(findArea)}`);
+
+// let radiusPara = document.querySelector('#radius');
+// radiusPara.textContent += ': ' + findArea
+
+// let resultPara = document.querySelector('#result');
+// resultPara.textContent += ': ' + circle(findArea)
+
+
+let shopItems = ["cheese","bread","pepper"];
+const shoppingListElement = document.querySelector(".shopping")
+
+const createItem = (shoppingItem) => {
+    for (let item of shoppingItem) {
+        let listItem = document.createElement('li');
+        listItem.textContent = item;
+        shoppingListElement.appendChild(listItem);
+    }
 }
 
-let findArea = prompt("Enter the radius");
-alert(`The area of a circle with radius ${findArea} is ${circle(findArea)}`);
-
-let radiusPara = document.querySelector('#radius');
-radiusPara.textContent += ': ' + findArea
-
-let resultPara = document.querySelector('#result');
-resultPara.textContent += ': ' + circle(findArea)
+createItem(shopItems)
