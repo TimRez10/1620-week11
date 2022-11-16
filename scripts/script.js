b57db3750d33adb6bab4875d24225e47dca81e61
Timur R <timurreziapov2@gmail.com>
2022-11-16 13:16:44 -0800

    defaults practice

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,22 @@
-const greet = (userName) => {
-    console.log(`Hello ${userName}`);
+// const greet = (userName) => {
+//     console.log(`Hello ${userName}`);
+//     return "123"
+// }
+// greet("Timur");
+// greet("Na");
+// greet(greet("Timur"));
+// greet(4223);
+
+// const greet = (userName = 'user', time = 'day') => {
+//     console.log(`Good ${time}, ${userName}`);
+// }
+// greet("Timur", "Night", "Dinner");
+// greet();
+
+const circle = (circleRadius) => {
+    let area = 2 * Math.PI * circleRadius
+    return area.toFixed(2)
 }
 
-greet("Timur");
-greet("Na");
+let findArea = prompt("Enter the radius")
+alert(`The area of a circle is ${circle(findArea)}`)
